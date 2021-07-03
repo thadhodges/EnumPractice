a39f4d8b627414f5286cc0db80f8b5fd294ba85e
Thad <thadisfree@gmail.com>
2021-07-02 22:57:21 -0500

    added chicken types enum

--- EnumPractice/EnumPractice/Program.cs
+++ EnumPractice/EnumPractice/Program.cs
@@ -10,12 +10,19 @@ namespace EnumPractice
         enum Foods
         {Chicken, Fries, Salad}
 
+        enum ChickenTypes
+        {Reds,
+        Bantham,
+        White,
+        Speckled}
+
 
         static void Main(string[] args)
         {
             
             Console.WriteLine(Weekdays.Tuesday);
             Console.WriteLine(Foods.Salad);
+            Console.WriteLine(ChickenTypes.Speckled);
         }
     }
 }
