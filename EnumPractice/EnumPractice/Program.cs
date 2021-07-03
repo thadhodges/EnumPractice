a5d0728e953fef5a6a152d0acca04f32fff2b06c
Thad <thadisfree@gmail.com>
2021-07-02 22:41:45 -0500

    initial setup

---
+++ EnumPractice/EnumPractice/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EnumPractice
+{
+    class Program
+    {
+        enum Weekdays
+        {Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday}
+
+        enum Foods
+        {Chicken, Fries, Salad}
+
+
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(Weekdays.Friday);
+            Console.WriteLine(Foods.Chicken);
+        }
+    }
+}
